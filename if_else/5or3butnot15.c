e488096ee1f4a6afce98d1cfdb04707f1e42f9f5
Rajnish Kumar <rajnish.kmr.dev@gmail.com>
2026-06-10 16:36:59 +0530

    revised

--- if_else/5or3butnot15.c
+++ if_else/5or3butnot15.c
@@ -17,6 +17,11 @@ int main()
     {
         printf("The number is not divisible by 5 or 3");
     }
-
+// if ((n%5==0 ||n%3==0) && (n%15!=0)){
+//     printf("the number is dibisible by 5 or 3 but not 15");
+// }
+// else{
+//     printf(" the number is not matching the required condition");
+// }
     return 0;
 }
--- if_else/divisibility5and3.c
+++ if_else/divisibility5and3.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter a number : ");
     scanf("%d", &n);
 
-    if (n % 5 == 0 && n % 3 == 0)
+    if ((n%5==0) && ( n%3==0))
     {
         printf("The number is divisible by 5 and 3");
     }
